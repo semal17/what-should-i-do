df7285e139a39ab317eeb966bcfd0a8e4a4fc384
Semen <senia-2010@mail.ru>
2022-05-06 10:55:36 +0300

    add click on button

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,27 +1,50 @@
+import { useState } from 'react';
+
 import './Item.css';
 
 
 
-function Item({ name, important = false }) {
+function Item({ name }) {
 
-    const style = {
-        color: important ? 'tomato' : 'black'
+    let [done, setDone] = useState(false);
+    let [imp, setImp] = useState(false);
+
+
+    let onNameClick = () => {
+        setDone(!done);
     };
+
+    let onImpClick = () => {
+        setImp(!imp);
+    };
+
+    
+
+    let styleName = 'item__name';
+
+    if (done) {
+        styleName += ' item__name--done';
+    }
+
+
+    if (imp) {
+        styleName += ' item__name--imp';
+    }
+
 
     return (
         <li className='item'>
             <div className='item__text'>
-                <p className='item__name' style={style}>{name}</p>
+                <p className={styleName} onClick={onNameClick}>{name}</p>
             </div>
             <div className='item__wrapper'>
-                <div className='item__icon'>
+                <div className='item__icon' onClick={onImpClick} >
                     <span className='item__icon-intro'>i</span>
                 </div>
                 <div className='item__icon'>
                     <span className='item__icon-intro'>d</span>
                 </div>
             </div>
-
         </li>
 
     );
